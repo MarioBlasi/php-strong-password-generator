cb69e4cfae0fc81da1afc8b14932a47e2444f982
MarioBlasi <mblasi509@gmail.com>
2023-05-05 18:06:41 +0100

    style

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  include __DIR__ . '/functions.php';
+include __DIR__ . '/functions.php';
 ?>
 
 <!DOCTYPE html>
@@ -17,81 +17,86 @@
     />
     <!--Script Bootstrap bundle-->
 </head>
-<body class="bg-secondary">
+<body class="bg-muted ">
   <header>
     <div class="container pt-5">
      <div class="row ">
        <div class="col d-flex justify-content-center  p-2 text-dark">
-         <div class="h1">Stong Password Generator</div>
+         <div class="h1"> <em>Stong Password Generator</em> </div>
        </div>
      </div>
      <div class="row">
-       <div class="col d-flex justify-content-center text-light p-2">
+       <div class="col d-flex justify-content-center text-secondary p-2">
          <div class="h2">Genera password sicura</div>
        </div>
      </div>
      <div class="row">
        <div class="col d-flex bg-info justify-content-center shadow p-2">
-         <div class="p">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure, cumque.</div>
+        <br>
        </div>
      </div>
    </div>
   </header>
   <!-- HEADER -->
-  <main class="rounded-2">
-    <div class="container ">
-      <div class="row shadow rounded-bottom-3">
+  <main >
+    <div class="container">
+      <div class="row shadow">
         <div class="col-6 d-flex justify-content-start flex-column p-2 text-secondary bg-light  ">
-          <div class="mb-5">
-            <p>consenti ripetizioni di uno o piú caratteri:</p>
+          <div class="mb-5 pt-3 text-center">
+            <p> <em>consenti ripetizioni di uno o piú caratteri:</em> </p>
           </div>
         </div>
         <!-- SECTION LEFT col-6 -->
 
         <div class="col-6 d-flex justify-content-start flex-column p-2 text-secondary bg-light  ">
-        <form method="GET" action="">
-          <div class="mb-3">
-            <label for="password" class="p-2">Lunghezza password:</label>
-            <input type="text" name="password" id="password" class="form-control w-50" placeholder="" value="<?php echo $password; ?>">
-          </div>
-          <button type="submit" class="btn btn-primary">Genera password</button>
-          <button type="reset" class="btn btn-secondary">Annulla</button>
-        </form>
-          <!-- password -->
-          <div class="form-check mt-2">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"checked>
-              <label class="form-check-label " for="flexRadioDefault1">
-                Si
-              </label>
-          </div>
-            <div class="form-check">
-              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
-              <label class="form-check-label" for="flexRadioDefault2">
-                No
-              </label>
+          <form method="GET">
+            <label for="password-length"><h5 class="pt-2">Inserisci la lunghezza della password: </h5> </label>
+            <input class="btn btn-primary btn-lg "  type="number" type="password" name="password" id="password-length" required min="1" max="100">
+            <div class="w-50 rounded" >
+              <button class="btn btn-secondary mt-2" type="submit">Genera password</button>
             </div>
-          </div>
+          </form>
+          <p><h5> La tua password casuale è: </h5><b> <h4> <?php echo $password; ?></h4></b></p>
+            <!-- password -->
+            <hr>
+            <div class=" d-flex flex-column  mb-2">
+              <div class="form-check">
+                <input class="form-check-input " type="checkbox" value="" id="flexCheckDefault"checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                  Lettere
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                  Numeri
+                </label>
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                <label class="form-check-label" for="flexCheckChecked">
+                Simboli
+                </label>
+              </div><!-- checkbox -->
+            </div>
+            <hr>
+            <div class="mb-3">
+              <div class="form-check ">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"checked>
+                  <label class="form-check-label " for="flexRadioDefault1">
+                    Si
+                  </label>
+              </div>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" >
+                  <label class="form-check-label" for="flexRadioDefault2">
+                    No
+                  </label>
+                </div>
+              </div>
+
+            </div>
           <!--radio-  -->
-          <div class="bg-info d-flex flex-column">
-            <div class="form-check mt-2">
-              <input class="form-check-input " type="checkbox" value="" id="flexCheckDefault"checked>
-              <label class="form-check-label" for="flexCheckDefault">
-                Lettere
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-              <label class="form-check-label" for="flexCheckChecked">
-                Numeri
-              </label>
-            </div>
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-              <label class="form-check-label" for="flexCheckChecked">
-               Simboli
-              </label>
-            </div><!-- checkbox -->
-          </div>
         </div> <!-- SECTION RIGHTcol-6 -->
       </div>
     </div>   
